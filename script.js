8deaf88ce2c7cc739dcc2bab79037eff0e92e9f4
Giovanni Luca Pinelli <giovannilucapinelli94@gmail.com>
2025-11-12 20:52:31 +0100

    Add files via upload
    * Added work, short break, and long break buttons (technically work, but have no adjustments made for resets and counter);
    * Skip button now disables when timer is not running. Should make sure to not disable it if the play button has been pressed at least once.
    * Added a progress bar, but not functional;

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 let minutes = 25;
 let seconds = 0;
-let pauseShort = 5;
-let pauseLong = 15;
 let isPaused = true;
+let isShortBreak = false;
+let isLongBreak = false;
 let timeInterval;
 let counter = 1;
+let timeElapsed = minutes * 60;
 
 const timer = document.getElementById("timer");
 const startTime = document.getElementById("startbtn");
 const skipTime = document.getElementById("skipbtn");
 const counterCont = document.getElementById("pomCounter");
 const resetCont = document.getElementById("resetCounter");
+const workTime = document.getElementById("work");
+const shortTime = document.getElementById("short");
+const longTime = document.getElementById("long");
 
 function formatTime(m, s) {
     return `${String(m).padStart(2, "0")}:${String(s).padStart(2, "0")}`;
 }
+
+workTime.addEventListener("click", () => {
+    timer.innerText = "25:00";
+    minutes = 25;
+})
+
+shortTime.addEventListener("click", () => {
+    timer.innerText = "05:00";
+    minutes = 5;
+    isShortBreak = true;
+})
+
+longTime.addEventListener("click", () => {
+    timer.innerText = "15:00";
+    minutes = 15;
+    isLongBreak = true;
+})
 
 function updateTime() {
     timer.innerText = formatTime(minutes, seconds);
@@ -35,36 +56,35 @@ function updateTime() {
 }
 
 startTime.addEventListener("click", () => {
-
     if (!isPaused) {
         clearInterval(timeInterval);
         timeInterval = "";
         isPaused = true;
-        startTime.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>`;
+        skipTime.disabled = true;
+        startTime.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#e3e3e3"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>`;
     } else {
         if (!timeInterval) {
             timeInterval = setInterval(updateTime, 1000);
             isPaused = false;
-            startTime.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M520-200v-560h240v560H520Zm-320 0v-560h240v560H200Zm400-80h80v-400h-80v400Zm-320 0h80v-400h-80v400Zm0-400v400-400Zm320 0v400-400Z"/></svg>`;
+            skipTime.disabled = false;
+            startTime.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#e3e3e3"><path d="M520-200v-560h240v560H520Zm-320 0v-560h240v560H200Zm400-80h80v-400h-80v400Zm-320 0h80v-400h-80v400Zm0-400v400-400Zm320 0v400-400Z"/></svg>`;
         }
     }
-
 });
 
 skipTime.addEventListener("click", () => {
-
-    if (timeInterval){
+    if (!isPaused) {
         clearInterval(timeInterval);
         timeInterval = "";
         timer.innerText = "25:00";
         minutes = 25;
         seconds = 0;
         isPaused = true;
+        skipTime.disabled = true;
         counter++;
         counterCont.innerText = +counter;
-        startTime.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>`;
+        startTime.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#e3e3e3"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>`;
     }
-
 });
 
 resetCont.addEventListener("click", () => {
